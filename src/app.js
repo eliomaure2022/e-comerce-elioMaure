4d46204243660b3ea067b889b0e9067c93eb1624
eliomaure2022 <elio_maure@yahoo.com.ar>
2023-02-06 15:14:17 -0300

    add dbsync

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
-// const db = require("./utils/database");
+const db = require("./utils/database");
 const error = require("./middlewares/error.middleware");
 const routerApi = require("./routes");
 
@@ -11,13 +11,13 @@ app.use(express.json());
 app.use(cors());
 app.use(morgan("tiny"));
 
-// db.authenticate()
-//   .then(() => console.log("Base de datos autenticada"))
-//   .catch((error) => console.log(error));
+db.authenticate()
+  .then(() => console.log("Base de datos autenticada"))
+  .catch((error) => console.log(error));
 
-// db.sync({ force: false })
-//   .then(() => console.log("Base de satos sincronizada"))
-//   .catch((error) => console.log(error));
+db.sync({ force: false })
+  .then(() => console.log("Base de satos sincronizada"))
+  .catch((error) => console.log(error));
 
 routerApi(app);
 
